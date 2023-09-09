131d13290104fcd15bb274cbb04e90ef3e827f1e
developeruche <developeruche@gmail.com>
2023-09-09 18:09:47 +0100

    mod: set-up file structure

---
+++ contracts/src/providers/database/pointers.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+library Pointers {
+    bytes32 constant LOTTERY_STORAGE_POSITION = keccak256("lottery.storage");
+    bytes32 constant ONCHAIN_ORACLE_STORAGE_POSITION = keccak256("onchain.oracle.storage");
+}
