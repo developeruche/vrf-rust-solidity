fdbc93766b968f20aa3e6f3a249dc943f6585336
developeruche <developeruche@gmail.com>
2023-09-10 17:04:20 +0100

    checkpoint: working on the lottery contract

--- contracts/src/modules/lottery.sol
+++ contracts/src/modules/lottery.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import {LotteryController} from "../controllers/lottery.controller.sol";
+import {AbstractOnchainOracle} from "./abstract-onchain-oracle.sol";
+import {OnchainOracleSchema} from "../providers/database/schemas/onchain-oracle.schema.sol";
+
 // start lottery 
 // end lottery
 // participate 
-contract Lottery {
+contract Lottery is AbstractOnchainOracle {
+
+    constructor(address _off_chain_signer) AbstractOnchainOracle(_off_chain_signer) {
+        
+    }
+
     function start() external {
         
     }   
@@ -16,4 +25,8 @@ contract Lottery {
     function end() external {
         
     }
+
+    function fullfill_randomness_future_internal(OnchainOracleSchema.Request memory request) internal override {
+        // set the reqest storage
+    } 
 }
