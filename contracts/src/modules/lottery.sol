4a41a1479e9173a0dea9023ab9b34f47d61f284a
developeruche <developeruche@gmail.com>
2023-09-10 15:35:04 +0100

    mod: started working on the example lottery contract

--- contracts/src/modules/lottery.sol
+++ contracts/src/modules/lottery.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-contract Lottery {}
+// start lottery 
+// end lottery
+// participate 
+contract Lottery {
+    function start() external {
+        
+    }   
+
+    function participate() external {
+        
+    }
+
+    function end() external {
+        
+    }
+}
