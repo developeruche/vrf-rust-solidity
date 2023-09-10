0b2b7ea0031fb36c7e6d988e5418c00f932513d9
developeruche <developeruche@gmail.com>
2023-09-10 05:35:04 +0100

    mod: done with the request schema

--- contracts/src/modules/onchain-oracle.sol
+++ contracts/src/modules/onchain-oracle.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import {Params} from "../providers/params.sol";
+
 contract OnchainOracle {
-    function request_for_randomness() external returns (bytes32) {
+    function request_for_randomness(Params.RandomnessRequest memory params) external returns (bytes32) {
         return bytes32(0);
     }
 
--- contracts/src/providers/errors/lottery.error.sol
+++ contracts/src/providers/errors/lottery.error.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-library LotteryErrors {}
+library LotteryErrors {
+    error REQUEST_ALREADY_EXISTS();
+    error INVALID_REQUEST_ID();
+}
